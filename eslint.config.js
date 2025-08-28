d05168c991a46b76f18182fe8a7ffac3d4fbac86
Dmitry Kuksinsky <dgk@dgk.su>
2025-08-29 02:09:19 +0400

    chore: migrate to typescript

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'old']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
