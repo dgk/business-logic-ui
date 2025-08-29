6a4a7b2deca2c47117e0832bec144bf1d9d4b77d
Dmitry Kuksinsky <dgk@dgk.su>
2025-08-29 12:50:56 +0400

    feat(interface): show loader and errors

--- src/features/interface/InterfaceList.tsx
+++ src/features/interface/InterfaceList.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
-import { Item, Segment } from 'semantic-ui-react'
+import { Item, Segment, Loader, Message } from 'semantic-ui-react'
 
 import ProgramCard from '../../components/ProgramCard'
 import { useRootStore } from '../../models'
@@ -15,16 +15,22 @@ const InterfaceList = observer(() => {
 
   return (
     <Segment>
-      <Item.Group divided>
-        {interfaceStore.data.map(item => (
-          <ProgramCard
-            key={item.id}
-            date={getFullDate(item.modification_time)}
-            title={item.title}
-            to={`/interface/${item.id}/program`}
-          />
-        ))}
-      </Item.Group>
+      {interfaceStore.isFetching && <Loader active />}
+      {!interfaceStore.isFetching && interfaceStore.error && (
+        <Message negative content={interfaceStore.error} />
+      )}
+      {!interfaceStore.isFetching && !interfaceStore.error && (
+        <Item.Group divided>
+          {interfaceStore.data.map(item => (
+            <ProgramCard
+              key={item.id}
+              date={getFullDate(item.modification_time)}
+              title={item.title}
+              to={`/interface/${item.id}/program`}
+            />
+          ))}
+        </Item.Group>
+      )}
     </Segment>
   )
 })
